98c348a1558504cd4f045e1fe0096e8d39fb78a7
githubqt <310176154@qq.com>
2016-08-31 00:26:00 +0800

    r

---
+++ health/bulid/js/Greeter.js
@@ -0,0 +1,30 @@
+//module.exports = function() {
+//    var greet = document.createElement('div');
+//    greet.textContent = "Hi there and greetings! hello";
+//    return greet;
+//};
+
+//var config = require('../../../config.json');
+//
+//module.exports = function() {
+//    var greet = document.createElement('div');
+//    greet.textContent = config.greetText;
+//    return greet;
+//};
+
+
+import React, {Component} from 'react'
+import config from '../../../config.json';
+import styles from '../../css/Greeter.css';//导入
+
+class Greeter extends Component{
+    render() {
+        return (
+            <div className={styles.root}>
+            {config.greetText}
+    </div>
+    );
+    }
+}
+
+export default Greeter
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,55 @@
 var webpack = require('webpack');
-var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+//var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
 
 module.exports = {
-    entry: {
-        entry1: './entry/entry1.js',
-        entry2: './entry/entry2.js'
-    },
+    devtool: 'eval-source-map',//配置打包测试
+    entry: __dirname + "/health/bulid/js/main.js",
     output: {
-        path: __dirname,
-        filename: '[name].entry.js'
+        path: __dirname + "/health/js",
+        filename: "bundle.js"
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            loader: 'babel-loader'
-        }, {
-            test: /\.jsx$/,
-            loader: 'babel-loader!jsx-loader?harmony'
-        }]
+        loaders: [
+            //{
+            //    test: /\.js$/,
+            //    loader: 'babel-loader'
+            //},
+            //{
+            //    test: /\.jsx$/,
+            //    loader: 'babel-loader!jsx-loader?harmony'
+            //},
+            {
+                test: /\.json$/,
+                loader: "json"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'//在webpack的module部分的loaders里进行配置即可
+                //以下代码可以在.babelrc中设置 webpack 可以加载编译
+                //query: {
+                //    presets: ['es2015', 'react']
+                //}
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css?modules!postcss'//添加对样式表的处理
+            }
+        ]
     },
-    plugins: [commonsPlugin]
+    plugins: [
+        //commonsPlugin
+        require('autoprefixer'),//调用autoprefixer插件
+        new webpack.BannerPlugin("Copyright zuiwang.win.")//在这个数组中new一个就可以了
+    ],
+    devServer: {
+        contentBase: "./health",//本地服务器所加载的页面所在的目录
+        colors: true,//终端中输出结果为彩色
+        historyApiFallback: true,//不跳转
+        inline: true,//实时刷新
+        port: 8000//实时刷新
+    }
 };
